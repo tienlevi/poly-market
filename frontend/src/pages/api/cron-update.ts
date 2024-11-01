df75dbde8cf3d2c6c2db8a842a459166a2fcdfff
TiennLevii <nguyentrachtien2401@gmail.com>
2024-11-01 09:48:10 +0700

    a

--- frontend/src/pages/api/cron-update.ts
+++ frontend/src/pages/api/cron-update.ts
@@ -12,7 +12,7 @@ export default async function GET(req: NextApiRequest, res: NextApiResponse) {
         "Content-Type": "application/json",
         Authorization: `Bearer ${process.env.NEXT_PUBLIC_CRON_SECRET}`,
       },
-      body: JSON.stringify({ title: title || "TienRonaldo" }),
+      body: JSON.stringify({ title: title || "TienNguyen" }),
     });
     const data = await response.json();
     return res.status(200).json(data);
