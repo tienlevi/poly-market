c525e8eefbf182c04526769091e0a635901596a7
TiennLevii <nguyentrachtien2401@gmail.com>
2024-11-01 09:40:39 +0700

    update: get method

--- frontend/src/pages/api/cron-update.ts
+++ frontend/src/pages/api/cron-update.ts
@@ -2,26 +2,21 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const polyMarketApi = process.env.NEXT_PUBLIC_POLY_MARKET as string;
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function GET(req: NextApiRequest, res: NextApiResponse) {
   const { title } = req.body;
 
-  if (req.method === "POST") {
-    try {
-      const response = await fetch(polyMarketApi, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.NEXT_PUBLIC_CRON_SECRET}`,
-        },
-        body: JSON.stringify({ title: title || "TienRonaldo" }),
-      });
-      const data = await response.json();
-      return res.status(200).json(data);
-    } catch (error) {
-      console.log(error);
-    }
+  try {
+    const response = await fetch(polyMarketApi, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_CRON_SECRET}`,
+      },
+      body: JSON.stringify({ title: title || "TienRonaldo" }),
+    });
+    const data = await response.json();
+    return res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
   }
 }
